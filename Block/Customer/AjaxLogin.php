436d383da34245113f226ddca8c5e8cea5610edb
Cuong NQ <bestearnmoney87@gmail.com>
2017-07-10 14:31:48 +0700

    init source code

---
+++ Block/Customer/AjaxLogin.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-07-10 11:01:03
+ * @Last Modified by:   nquangcuong
+ * @Last Modified time: 2017-07-10 14:28:40
+ * @website: http://giaphugroup.com
+ */
+
+namespace PHPCuong\CustomerAjaxLogin\Block\Customer;
+
+class AjaxLogin extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var \Magento\Customer\Model\Url
+     */
+    protected $_customerUrl;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Customer\Model\Url $customerUrl
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\Url $customerUrl,
+        \Magento\Framework\App\Http\Context $httpContext,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_customerUrl = $customerUrl;
+        $this->httpContext = $httpContext;
+    }
+
+    /**
+     * Retrieve password forgotten url
+     *
+     * @return string
+     */
+    public function getForgotPasswordUrl()
+    {
+        return $this->_customerUrl->getForgotPasswordUrl();
+    }
+
+    /**
+     * Retrieve ajaxlogin url
+     *
+     * @return string
+     */
+    public function getAjaxLoginUrl()
+    {
+        return $this->getUrl('phpcuong/customer/ajaxlogin');
+    }
+
+    /**
+     * Checking customer login status
+     *
+     * @return bool
+     */
+    public function customerLoggedIn()
+    {
+        return (bool)$this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+    }
+}
---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-07-10 10:55:59
+ * @Last Modified by:   nquangcuong
+ * @Last Modified time: 2017-07-10 10:56:12
+ * @website: http://giaphugroup.com
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_AjaxLogin',
+    __DIR__
+);
