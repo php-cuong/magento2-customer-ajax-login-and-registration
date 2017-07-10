c813876d0a29e87ab82cbad632812eb216113838
Cuong NQ <bestearnmoney87@gmail.com>
2017-07-10 15:13:52 +0700

    update

--- registration.php
+++ registration.php
@@ -4,12 +4,12 @@
  * @Author: Ngo Quang Cuong
  * @Date:   2017-07-10 10:55:59
  * @Last Modified by:   nquangcuong
- * @Last Modified time: 2017-07-10 10:56:12
+ * @Last Modified time: 2017-07-10 15:05:28
  * @website: http://giaphugroup.com
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PHPCuong_AjaxLogin',
+    'PHPCuong_CustomerAjaxLogin',
     __DIR__
 );
